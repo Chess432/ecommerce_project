a2f97db7e6c6f841a900b3d24516f41e2914ef88
Chess432 <mchess432@gmail.com>
2019-12-06 21:37:54 +0300

    created footer, navbar and base files

--- core/views.py
+++ core/views.py
@@ -5,4 +5,17 @@ def item_list(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, "home-page.html", context)
+    return render(request, "home.html", context)
+
+
+def checkout(request):
+    context = {
+        "Title": "Checkout"
+    }
+    return render(request, "checkout.html", context)
+
+def products(request):
+    context = {
+        "title":"Products"
+    }
+    return render(request, "product.html", context)
